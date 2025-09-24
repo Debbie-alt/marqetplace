a9d87ba711488e3464e5503f7778e8357acaa653
Debbie-alt <funmmy565@gmail.com>
2025-09-24 12:26:33 +0100

    Hero Section

---
+++ src/app/components/Homepage/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='bg-[#7AD5EC] px-16 py-10 w-screen'>
+        <div>
+            <h1 className='font-extrabold text-5xl text-black '>SPIN IT. VERIFY IT. TRUST IT.</h1>
+        </div>
+
+        <div className='flex flex-col '>
+            
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
