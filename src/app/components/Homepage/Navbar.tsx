385db6171575715c1b7b9cad3d23a8571b73bc7a
Debbie-alt <funmmy565@gmail.com>
2025-09-24 11:43:55 +0100

    Navbar Component

--- src/app/components/Homepage/Navbar.tsx
+++ src/app/components/Homepage/Navbar.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Navbar = () => {
+const Navbar = () => {      
   return (
-    <div>Navbar</div>
+     <nav className='w-full flex items-center justify-between px-16 py-4 '>
+      <div className='logo flex gap-3 items-center'>
+        <div className='px-3 py-2 font-semibold text-3xl bg-[#2D2C2C] text-[#8FD7ED]'>M</div>
+        <h3 className='text-2xl font-semibold'>Marqetplace</h3>
+
+      </div>
+      <div className='nav flex items-center gap-3'>
+        <Link href="/store" className='cursor-pointer'>Store</Link>
+        <Link href="/about" className='cursor-pointer'>Tech</Link>
+        <Link href="/contact" className='cursor-pointer'>Support</Link>
+       </div>
+
+      <div className='signup flex items-center gap-3'>
+        <Link  href="/signup" className='cursor-poitorenter'>Sign up</Link>
+        <Link  href="/signup" className='cursor-pointer'>Sign in</Link>
+
+    </div>
+     </nav>
   )
 }
 
-export default Navbar
+export default Navbar 
