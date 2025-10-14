1a339eb065b6e815b2f4f9aec6c32dddbb3e52b9
Debbie-alt <funmmy565@gmail.com>
2025-10-14 06:36:29 +0100

    Blend brightness

--- src/app/components/Homepage/Hero.tsx
+++ src/app/components/Homepage/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
         alt="hero image" 
         width={1300}
         height={200}
-        className="object-contain mt-5 opacity-80 mix-blend-burn" 
+        className="object-contain mt-5 opacity-80 mix-blend-normal brightness-96" 
         priority
       />            
 
